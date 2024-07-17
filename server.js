b9b48704ec9766e7fe195acd3bf945f56ba4982d
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-07-16 22:02:08 -0400

    project done, work on the delete

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const html_routes = require('./routes/html-routes')
 const api_routes = require('./routes/api-routes')
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 
 const app = express();
 
 // express middleware will always run the operation in the order from
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use("/api", api_routes)
+app.use("/", html_routes)
+
 
 app.listen(PORT, () => {
-    console.log(`Server running on `)
+    console.log(`Server running on`)
 }) ;
